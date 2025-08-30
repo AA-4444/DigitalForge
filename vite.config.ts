ca1786a38b5e0e36c303d941268c4fdbc4765ad2
Oleksii Zarytskyi <104495043+AA-4444@users.noreply.github.com>
2025-08-30 06:10:25 +0200

    update app

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/DigitalForge/", // <-- важно для GitHub Pages
+  base: "/DigitalForge/", 
   server: { host: "::", port: 8080 },
   plugins: [react()],
   resolve: {
